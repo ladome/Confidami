29ad774fdc7ae37985cfbcc20ad2847864965b26
ladome <domenicolafauci@gmail.com>
2015-07-29 00:02:45 +0200

    Inserimento campo userid in tabella tblposts + script di alter

--- DB/change_27072015.sql
+++ DB/change_27072015.sql
@@ -1,3 +1,3 @@
 ALTER TABLE tblPostsAttachments ADD Timestamp datetime NULL;
 ALTER TABLE tblTempAttachments ADD Timestamp datetime NULL;
-ALTER TABLE tblPosts ADD Userid datetime default ('00000000-0000-0000-0000-000000000000');
+ALTER TABLE tblPosts ADD Userid varchar(50) default ('00000000-0000-0000-0000-000000000000');
